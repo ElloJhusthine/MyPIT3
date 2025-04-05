6ea03d5f683d2d96aaa6448926600b23f846a46b
ElloJhusthine <jhusthine.ello@gmail.com>
2025-04-06 00:08:04 +0800

    Fix API_URL for deployed backend

--- front/src/api.js
+++ front/src/api.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const API_URL = "https://mypit3.onrender.com/api/tasks/";
 
-
 export const getTasks = () => axios.get(API_URL);
 export const addTask = (task) => axios.post(API_URL, task);
 export const updateTask = (id, task) => axios.put(`${API_URL}${id}/`, task);
