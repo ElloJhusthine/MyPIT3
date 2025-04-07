4ce469975cab334a310e06597524e88006f49be5
ElloJhusthine <jhusthine.ello@gmail.com>
2025-04-07 20:28:28 +0800

    Auto-create superuser on deploy

--- Back/todo/create_super.py
+++ Back/todo/create_super.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 
 def create_superuser():
     if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser("admin", "admin@gmail.com", "Jhusthine@123")
+        User.objects.create_superuser("admin", "admin@gmail.com", "hateu123")
         print("Superuser created")
     else:
         print("Superuser already exists")
